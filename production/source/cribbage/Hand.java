e1b5dc79ebba5dafd7fb9c87f59398a50c551261
elisavitale <elisavitale@live.it>
2019-12-04 10:00:59 +0100

    Created class Hand.

---
+++ production/source/cribbage/Hand.java
@@ -0,0 +1,15 @@
+package cribbage;
+
+public class Hand {
+
+    private String hand;
+
+    public Hand(String hand) {
+        this.hand = hand;
+    }
+
+    public static int score() {
+        return 0;
+    }
+
+}
